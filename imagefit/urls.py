0259dd5683f466aac3de8e63c7c387d495c59ccf
Serdar DURBARIŞ <serdardurbaris@gmail.com>
2020-05-18 00:00:08 +0300

    Change url

--- imagefit/urls.py
+++ imagefit/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, re_path
 from . import views
 
+"""
+Master URL using
+  path('image', include('imagefit.urls')),
+"""
 
 urlpatterns = [
     url(
-        r'^(?P<path_name>[\w_-]*)/(?P<format>[,\w-]+)/(?P<url>.*)/?$',
+        r'^(?P<url>.*)/(?P<format>[,\w-]+)/(?P<path_name>[\w_-]*)/?$',
         views.resize,
         name="imagefit_resize"),
 ]
